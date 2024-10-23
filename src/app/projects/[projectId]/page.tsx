784da503b675302d5c84158b1e0007a2d125bcc0
kpzip <kpzipgames@gmail.com>
2024-10-23 13:16:56 -0700

    w

--- src/app/projects/[projectId]/page.tsx
+++ src/app/projects/[projectId]/page.tsx
@@ -1,6 +1,13 @@
+import Projects from "./public/data.json";
+
 export default function project({ params }: { params: { projectId: string } }) {
   const name = params.projectId;
+  const info = Projects.projects[name].info
   return (
-    <h1>Project Name: {name}</h1>
+    <div>
+      <h1>Project Name: {name}</h1>
+      <p>{info}</p>
+    </div>
   );
-} 
+}
+
